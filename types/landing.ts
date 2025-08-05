872073fe0a72c8a5ba66a35cc32128bdec005cf4
Ruth Ereso <ruthfian74@gmail.com>
2025-08-05 09:21:37 +0300

    feat: add landing page

---
+++ types/landing.ts
@@ -0,0 +1,13 @@
+
+export interface JourneyStep {
+    logo: string; 
+    phase:string
+    description: string;
+  }
+  
+  export interface Testimonial {
+    photo: string;
+    name: string;
+    role: string;
+    description: string;
+  }
