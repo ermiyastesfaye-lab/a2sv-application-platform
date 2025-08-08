ea335131966a00206ee0bba7e9359079a5c18c82
namizz <139370392+namizz@users.noreply.github.com>
2025-08-08 10:25:37 +0300

    admin api slice mergeing on one file

--- lib/redux/api/adminApi.ts
+++ lib/redux/api/adminApi.ts
@@ -3,44 +3,54 @@
 //   CreateCycleRequest,
 //   CycleResponse,
 // } from "@/lib/types/applicationCycles";
-// import type { RootState } from "@/lib/redux/store";
 
+// export const adminApi = createApi({
+//   reducerPath: "adminApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://a2sv-application-platform-backend-team1.onrender.com/",
+//     prepareHeaders: (headers, { getState }) => {
+//       const token = localStorage.getItem("token");
+//       if (token) {
+//         headers.set("Authorization", `Bearer ${token}`);
+//       }
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     createCycle: builder.mutation<
+//       { success: boolean; data: CycleResponse },
+//       CreateCycleRequest
+//     >({
+//       query: (newCycle) => ({
+//         url: "/admin/cycles",
+//         method: "POST",
+//         body: newCycle,
+//       }),
+//     }),
+//     activateCycle: builder.mutation<
+//       { success: boolean; message: string },
+//       { cycleId: string }
+//     >({
+//       query: ({ cycleId }) => ({
+//         url: `/admin/cycles/${cycleId}/activate`,
+//         method: "PATCH",
+//       }),
+//     }),
 
-export const adminApi = createApi({
-  reducerPath: 'adminApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://a2sv-application-platform-backend-team1.onrender.com/',
-    prepareHeaders: (headers, { getState }) => {
-     
-      const token = localStorage.getItem('token');
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
-  endpoints: (builder) => ({
-    createCycle: builder.mutation<{ success: boolean; data: CycleResponse }, CreateCycleRequest>({
-      query: (newCycle) => ({
-        url: '/admin/cycles',
-        method: 'POST',
-        body: newCycle,
-      }),
-    }),
-    activateCycle: builder.mutation<{ success: boolean; message:string}, {cycleId:string}>({
-      query: ({cycleId}) => ({
-        url: `/admin/cycles/${cycleId}/activate`,
-        method: 'PATCH'
-      }),
-    }),
+//     deleteCycle: builder.mutation<
+//       { success: boolean; message: string },
+//       { cycleId: string }
+//     >({
+//       query: ({ cycleId }) => ({
+//         url: `/admin/cycles/${cycleId}`,
+//         method: "DELETE",
+//       }),
+//     }),
+//   }),
+// });
 
-    deleteCycle: builder.mutation<{ success: boolean; message:string}, {cycleId:string}>({
-      query: ({cycleId}) => ({
-        url: `/admin/cycles/${cycleId}`,
-        method: 'DELETE'
-      }),
-    }),
-  }),
-});
-
-export const { useCreateCycleMutation, useActivateCycleMutation,useDeleteCycleMutation } = adminApi;
+// export const {
+//   useCreateCycleMutation,
+//   useActivateCycleMutation,
+//   useDeleteCycleMutation,
+// } = adminApi;
