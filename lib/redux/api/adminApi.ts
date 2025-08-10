aacaf115b347029daea4506a731672ec41d8fea1
fikante <fikiranteneh920@gmail.com>
2025-08-10 16:00:34 +0300

    Revert unwanted file changes

--- lib/redux/api/adminApi.ts
+++ lib/redux/api/adminApi.ts
@@ -1,9 +1,61 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import {
-  CreateCycleRequest,
-  CycleResponse,
-} from "@/lib/types/applicationCycles";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import {
+//   CreateCycleRequest,
+//   CycleResponse,
+// } from "@/lib/types/applicationCycles";
 
+<<<<<<< Updated upstream
+// export const adminApi = createApi({
+//   reducerPath: "adminApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://a2sv-application-platform-backend-team1.onrender.com/",
+//     prepareHeaders: (headers, { getState }) => {
+//       const token = localStorage.getItem("token");
+//       if (token) {
+//         headers.set("Authorization", `Bearer ${token}`);
+//       }
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     createCycle: builder.mutation<
+//       { success: boolean; data: CycleResponse },
+//       CreateCycleRequest
+//     >({
+//       query: (newCycle) => ({
+//         url: "/admin/cycles",
+//         method: "POST",
+//         body: newCycle,
+//       }),
+//     }),
+//     activateCycle: builder.mutation<
+//       { success: boolean; message: string },
+//       { cycleId: string }
+//     >({
+//       query: ({ cycleId }) => ({
+//         url: `/admin/cycles/${cycleId}/activate`,
+//         method: "PATCH",
+//       }),
+//     }),
+
+//     deleteCycle: builder.mutation<
+//       { success: boolean; message: string },
+//       { cycleId: string }
+//     >({
+//       query: ({ cycleId }) => ({
+//         url: `/admin/cycles/${cycleId}`,
+//         method: "DELETE",
+//       }),
+//     }),
+//   }),
+// });
+
+// export const {
+//   useCreateCycleMutation,
+//   useActivateCycleMutation,
+//   useDeleteCycleMutation,
+// } = adminApi;
+=======
 export const adminApi = createApi({
   reducerPath: 'adminApi',
   baseQuery: fetchBaseQuery({
@@ -42,4 +94,4 @@ export const adminApi = createApi({
 });
 
 export const { useCreateCycleMutation, useActivateCycleMutation,useDeleteCycleMutation } = adminApi;
-
+>>>>>>> Stashed changes
