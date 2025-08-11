441f615bd697aae1007a80bccca5efb3d1e0ad29
fikante <fikiranteneh920@gmail.com>
2025-08-11 10:01:17 +0300

    solve issues on the evaluation form

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -5,9 +5,9 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 // import { adminApi } from "./api/adminApi";
 import { adminApi } from "./slices/adminSlice";
 import { cyclesApi } from "./api/applicationCyclesApi";
-// import { reviewerApi } from "./api/reviewerApi";
+import { reviewerApi } from "./api/reviewerApi";
 import { profileApi } from "./api/profileApi";
-// import reviewerReducer from "./slices/reviewerSlice";
+import reviewerReducer from "./slices/reviewerSlice";
 import profileReducer from "./slices/profileSlice";
 
 import { managersApi } from "./api/managerApi";
@@ -25,25 +25,20 @@ export const store = configureStore({
 
     [managersApi.reducerPath]: managersApi.reducer,
 
-    //     [reviewerApi.reducerPath]: reviewerApi.reducer,
+    [reviewerApi.reducerPath]: reviewerApi.reducer,
     [profileApi.reducerPath]: profileApi.reducer,
-    //     reviewer: reviewerReducer,
-    //     profile: profileReducer,
-    // >>>>>>> main
+    reviewer: reviewerReducer,
+    profile: profileReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       authApi.middleware,
       adminApi.middleware,
       cyclesApi.middleware,
-
       managersApi.middleware,
       applicationsApi.middleware,
-
-      //
-      //       reviewerApi.middleware,
+      reviewerApi.middleware,
       profileApi.middleware
-      //
     ),
 });
 
